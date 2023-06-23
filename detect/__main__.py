011293ddb2a21ec5baa975e21a4f9c0a966fbedc
Lance Hartung <lhartung@wisc.edu>
2023-06-22 21:09:10 -0500

    Try creating map features from detected objects

--- detect/__main__.py
+++ detect/__main__.py
@@ -1,3 +1,5 @@
+import collections
+import math
 import operator
 import os
 import sys
@@ -12,6 +14,9 @@ VIZAR_SERVER = os.environ.get("VIZAR_SERVER", "localhost:5000")
 
 MODEL_REPO = "custom"
 MODEL_NAME = "yolov8n-seg-c04-nms"
+
+MARK_LABELS = set(["door", "dining table"])
+MIN_DISTANCE = 1
 
 
 def repair_images():
@@ -34,6 +39,57 @@ def repair_images():
         change = {"status": status}
         url = "http://{}/photos/{}".format(VIZAR_SERVER, item['id'])
         requests.patch(url, json=change)
+
+
+def try_create_features(location_id, item, info):
+    # Get current list of features for the location
+    features_url = "http://{}/locations/{}/features".format(VIZAR_SERVER, location_id)
+    response = requests.get(features_url)
+    if not response.ok:
+        return
+
+    features = response.json()
+    features_by_label = collections.defaultdict(list)
+
+    # Organize the existing features by name.
+    # We are only interested in objects of the configured types.
+    for feature in features:
+        if feature.get("type") == "object" and feature.get("name") in MARK_LABELS:
+            features_by_label[feature['name']].append(feature)
+
+    for obj in info.get("annotations", []):
+        if obj['label'] not in MARK_LABELS:
+            continue
+
+        pos = obj.get("position")
+        if pos is None:
+            continue
+
+        # Check if there is any already existing feature within configured minimum radius.
+        # If so, we will not create another feature.
+        duplicate = False
+        for other in features_by_label[obj['label']]:
+            sq_dist = sum( (pos[d] - other['position'][d])**2 for d in ["x", "y", "z"] )
+            if math.sqrt(sq_dist) < MIN_DISTANCE:
+                duplicate = True
+                break
+
+        if duplicate:
+            continue
+
+        # Create a new feature on the map
+        new_feature = {
+            "name": obj['label'],
+            "position": pos,
+            "style": {
+                "placement": "point"
+            },
+            "type": "object"
+        }
+        response = requests.post(features_url, json=new_feature)
+        if response.ok:
+            new_feature = response.json()
+            features_by_label[obj['label']].append(new_feature)
 
 
 def main():
@@ -73,6 +129,10 @@ def main():
                 geom_url = "{}/geometry.png".format(url)
                 if result.try_localize_objects(geom_url):
                     requests.patch(url, json=result.info)
+
+                    camera_location_id = item.get("camera_location_id")
+                    if camera_location_id is not None:
+                        try_create_features(camera_location_id, item, result.info)
 
             else:
                 info = {"status": "error"}
